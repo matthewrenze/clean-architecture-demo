a359d35daa8417451fc20b50965fe61a36a0e693
Matthew Renze <matthew.renze@gmail.com>
2015-12-06 19:58:57 -0600

    Added unit tests to domain

---
+++ Domain/Sales/SaleTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CleanArchitecture.Domain.Sales
+{
+    [TestFixture]
+    public class SaleTests
+    {
+        private Sale _sale;
+
+        private const decimal UnitPrice = 1.00m;
+        private const int Quantity = 1;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sale = new Sale()
+            {
+                UnitPrice = UnitPrice,
+                Quantity = Quantity
+            };
+        }
+
+        [Test]
+        public void TestSetUnitPriceShouldRecomputeTotalPrice()
+        {
+            _sale.UnitPrice = 1.23m;
+
+            Assert.That(_sale.TotalPrice, Is.EqualTo(1.23));
+        }
+
+        [Test]
+        public void TestSetQuantityShouldRecomputeTotalPrice()
+        {
+            _sale.Quantity = 2;
+
+            Assert.That(_sale.TotalPrice, Is.EqualTo(2.00m));
+        }
+    }
+}
