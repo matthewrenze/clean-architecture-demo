47974a48f6f99ff043f0d45f2c053771d654dbce
Matthew Renze <matthew.renze@gmail.com>
2017-02-03 13:44:18 -0800

    Added Unit of Work

---
+++ Persistance/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Application.Interfaces;
+
+namespace CleanArchitecture.Persistance
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDatabaseService _database;
+
+        public UnitOfWork(IDatabaseService database)
+        {
+            _database = database;
+        }
+
+        public void Save()
+        {
+            _database.Save();
+        }
+    }
+}
---
+++ Persistance/UnitOfWorkTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMoq;
+using NUnit.Framework;
+
+namespace CleanArchitecture.Persistance
+{
+    [TestFixture]
+    public class UnitOfWorkTests
+    {
+        private UnitOfWork _unitOfWork;
+        private AutoMoqer _mocker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mocker = new AutoMoqer();
+
+            _unitOfWork = _mocker.Create<UnitOfWork>();
+        }
+
+        [Test]
+        public void TestSaveShouldSaveUnitOfWork()
+        {
+            _unitOfWork.Save();
+        }
+    }
+}
